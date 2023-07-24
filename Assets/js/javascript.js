3698496a7ff8da5905a844f904c51f29f3822e1e
Muhammad Mohsin <mmohsin@cymru.com>
2023-07-24 10:14:34 -0400

    created files and a layout of the site

---
+++ Assets/js/javascript.js
@@ -0,0 +1,8 @@
+var submitEl = document.querySelector("#submit");
+function start_game(event) {
+    event.preventDefault();
+    console.log("Testing");
+}
+addEventListener("click"){
+ console.log("testing");
+}
